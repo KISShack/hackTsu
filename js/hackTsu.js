149a4b5f679d1d56173f5f10f08b044fc0e67617
oto <otololua@gmail.com>
2015-03-20 23:35:29 +0400

    add tabs actives
    add tab actives

--- js/hackTsu.js
+++ js/hackTsu.js
@@ -1,9 +1,21 @@
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+$(document).ready(function() {
+    $(function () {
+        $('a.page-scroll').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 40
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    $(function () {
+        $('#main-nav a').bind('click', function (event) {
+            var $navtab = $('.tab-attr');
+            if($navtab.hasClass('active')) {
+                $navtab.removeClass('active')
+            }
+            $(this).addClass('active');
+        });
     });
 });
