05950dcdcc01325f479a9acf5d7f0a91308e9d18
oto <otololua@gmail.com>
2015-03-20 13:52:52 +0400

    speakers, scrolling animation
    add speakers and scrolling animation

--- js/hackTsu.js
+++ js/hackTsu.js
@@ -1,8 +1,8 @@
-$(function () {
-    $('a.page-scroll').bind('click', function(event){
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        $('html,body').animate({
-            scrollTop: $($anchor.attr('href').offset().top)
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
