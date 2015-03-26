6edf45cf92795556a9d33d0731bac95126144260
Levani <Levani.Lekishvili@gmail.com>
2015-03-26 23:21:28 +0400

    agenda hover

--- js/agenda.js
+++ js/agenda.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     //$('#3_3').remove();
-
+        $('.has-events div').hover(
+            function(){
+                $(this).addClass('addborder');
+            },
+            function(){
+                $(this).removeClass('addborder');
+            })
 
 });
