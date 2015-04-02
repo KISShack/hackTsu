9df620c6be2df91c0c4982c5bd7a75f935e9721b
Levani <Levani.Lekishvili@gmail.com>
2015-04-02 13:37:41 +0400

    nav bar
    nav bar hower effect

--- js/hackTsu.js
+++ js/hackTsu.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(function () {
         $('a.page-scroll').bind('click', function (event) {
             var $anchor = $(this);
@@ -9,45 +9,45 @@ $(document).ready(function() {
         });
     });
 
+    //$(function () {
+    //    $('.speaker').hover(
+    //        function () {
+    //            $(this).addClass('speaker-over');
+    //
+    //        },
+    //        function () {
+    //            $(this).removeClass('speaker-over');
+    //        }
+    //    );
+    //});
+
     $(function () {
-        $('.speaker').hover(
-            function(){
-                $(this).addClass('speaker-over');
-
-            },
-            function(){
-                $(this).removeClass('speaker-over');
-            }
-        );
-    });
-
-    $(function(){
-        $('nav#main-nav a').hover(
-            function(){
+        $('nav#main-nav li').hover(
+            function () {
                 $(this).addClass('nav-item-hover');
             },
-            function(){
+            function () {
                 $(this).removeClass('nav-item-hover');
             }
         );
     });
-    var mouseX;
+   /* var mouseX;
     var mouseY;
-    $(document).mousemove( function(e) {
+    $(document).mousemove(function (e) {
         mouseX = e.pageX;
         mouseY = e.pageY;
-       //$('.circle').animate({top:mouseY,left:mouseX},1);
+        //$('.circle').animate({top:mouseY,left:mouseX},1);
     });
-    $(".home").mousemove(function(){
-        console.log(mouseY,mouseX)
-        $('.circle').css({top:mouseY,left:mouseX});
+    $(".home").mousemove(function () {
+        console.log(mouseY, mouseX)
+        $('.circle').css({top: mouseY, left: mouseX});
     });
-    $('.home').mouseout(function(){
+    $('.home').mouseout(function () {
         $('.circle').fadeOut("slow");
     })
-    $('.home').hover(function(){
+    $('.home').hover(function () {
         $('.circle').fadeIn("slow");
-    })
+    })*/
 
 
 });
